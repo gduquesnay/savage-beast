4bcd05a3597affa11c466b2ebb8c40934ed413b4
Pavel Valodzka <pavel@valodzka.name>
2009-11-14 15:11:24 +0200

    finish generator

--- generators/beast/beast_generator.rb
+++ generators/beast/beast_generator.rb
@@ -4,23 +4,28 @@ class BeastGenerator < Rails::Generator::Base
   
   def manifest
     record do |m|
-      m.migration_template "migrations/create_savage_tables.rb", 'db/migrate', :migration_file_name => "create_savage_tables"
-
       destination_root = "/public/"
       templates_dir = (Pathname(__FILE__).dirname + 'templates').expand_path
       
-      %w(stylesheets images javascripts).each{|asset|
+      %w(stylesheets images javascripts).each { |asset|
         destination_path = destination_root + asset
-        m.dir destination_path + "/savage_beast"
+        assets_dir = templates_dir + 'public' + asset
         
-        Dir[templates_dir + 'public' + asset + '*'].each do |f|
-          from_file = Pathname(f).relative_path_from(templates_dir)
-          dirname, basename = from_file.split
-          m.file(file, dirname + 'savage_beast' + basename)
+        m.directory destination_path + "/savage_beast"
+
+        Pathname.glob(assets_dir + '**' + '*') do |file|
+          destination_file = (assets_dir + 'savage_beast' + file.relative_path_from(assets_dir)).relative_path_from(templates_dir).to_s
+          source_file = file.relative_path_from(templates_dir).to_s
+          
+          if file.directory?
+            m.directory destination_file
+          else
+            m.file source_file, destination_file
+          end
         end
       }
-      
-      m.file "public/javascripts/beast.js", "public/javascripts/beast.js"
+
+      m.migration_template "migrations/create_savage_tables.rb", 'db/migrate', :migration_file_name => "create_savage_tables"
     end
   end
 end
