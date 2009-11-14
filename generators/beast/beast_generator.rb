76ebc7dfab5a5ac7d382076d75f077ecedc08f0e
Pavel Valodzka <pavel@valodzka.name>
2009-11-14 14:00:27 +0200

    move all assets to generator

--- generators/beast/beast_generator.rb
+++ generators/beast/beast_generator.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 
 class BeastGenerator < Rails::Generator::Base
   
@@ -5,6 +6,20 @@ class BeastGenerator < Rails::Generator::Base
     record do |m|
       m.migration_template "migrations/create_savage_tables.rb", 'db/migrate', :migration_file_name => "create_savage_tables"
 
+      destination_root = "/public/"
+      templates_dir = (Pathname(__FILE__).dirname + 'templates').expand_path
+      
+      %w(stylesheets images javascripts).each{|asset|
+        destination_path = destination_root + asset
+        m.dir destination_path + "/savage_beast"
+        
+        Dir[templates_dir + 'public' + asset + '*'].each do |f|
+          from_file = Pathname(f).relative_path_from(templates_dir)
+          dirname, basename = from_file.split
+          f.file(file, dirname + 'savage_beast' + basename)
+        end
+      }
+      
       m.file "public/javascripts/beast.js", "public/javascripts/beast.js"
     end
   end
