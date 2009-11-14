3b20d5da30974c730745a9161b1be5039395465b
Pavel Valodzka <pavel@valodzka.name>
2009-11-14 13:38:44 +0200

    add js to generator

--- generators/beast/beast_generator.rb
+++ generators/beast/beast_generator.rb
@@ -4,6 +4,8 @@ class BeastGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template "migrations/create_savage_tables.rb", 'db/migrate', :migration_file_name => "create_savage_tables"
+
+      m.file "public/javascripts/beast.js", "public/javascripts/beast.js"
     end
   end
 end
